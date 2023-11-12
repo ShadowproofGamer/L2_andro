a2aca179352b97bb31e9b1b5fd64e021c69dc89b
Shadowproof <kubac400@gmail.com>
2023-11-12 17:35:30 +0100

    Error in menu doesn't show up

--- app/src/main/java/com/example/l2_andro/MainActivity.kt
+++ app/src/main/java/com/example/l2_andro/MainActivity.kt
@@ -16,81 +16,98 @@ class MainActivity : AppCompatActivity() {
     lateinit var button2: Button
     lateinit var button3: Button
     lateinit var toolbar1: Toolbar
-    lateinit var menu1: Menu
+
     //fun OnCreateOptionsMenu(menu: Menu!):Boolean
-    fun applyTheme(){
+    fun applyTheme() {
         val data: SharedPreferences = getSharedPreferences("my_prefs", Context.MODE_PRIVATE)
         var themeNum = data.getInt("theme_color", 0)
-        when (themeNum){
+        when (themeNum) {
             1 -> setTheme(R.style.AppThemeRed)
             2 -> setTheme(R.style.AppThemeGreen)
             3 -> setTheme(R.style.AppThemeGrey)
             else -> setTheme(R.style.Theme_L2_andro)
         }
     }
-    fun setPrefs(themeNum:Int){
+
+    fun setPrefs(themeNum: Int) {
         val data: SharedPreferences = getSharedPreferences("my_prefs", Context.MODE_PRIVATE)
         val editor = data.edit()
         editor.putInt("theme_color", themeNum)
         editor.apply()
 
     }
-
-    override fun OnCreateOptionsMenu(menu: Menu):Boolean{
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.app_menu, menu)
         return true
     }
-    override fun onOptionsItemSelected(item: MenuItem):Boolean{
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
-            case R.id.mi0 -> {
+            R.id.mi0 -> {
+                setPrefs(0)
+                recreate()
                 true
             }
-            case R.id.mi1 -> {
+
+            R.id.mi1 -> {
+                setPrefs(1)
+                recreate()
                 true
             }
-            case R.id.mi2 -> {
+
+            R.id.mi2 -> {
+                setPrefs(2)
+                recreate()
                 true
             }
-            case R.id.mi3 -> {
+
+            R.id.mi3 -> {
+                setPrefs(3)
+                recreate()
                 true
             }
+
             else -> super.onContextItemSelected(item)
         }
     }
 
-                override fun onCreate(savedInstanceState: Bundle?) {
-                    super.onCreate(savedInstanceState)
-                    applyTheme()
-                    binding = ActivityMainBinding.inflate(layoutInflater)
-                    setContentView(binding.root)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        applyTheme()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-                    setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main)
 
-                    toolbar1 = binding.toolbar1
-                    setSupportActionBar(toolbar1)
+        toolbar1 = binding.toolbar1
+        setSupportActionBar(toolbar1)
 
-                    button1 = binding.button1
-                    button1.setOnClickListener { _ ->
-                        println("button1 clicked")
-                        setPrefs(1)
-                        recreate()
-                    }
+        //onCreateOptionsMenu(toolbar1)
 
-                    button2 = binding.button2
-                    button2.setOnClickListener { _ ->
-                        setPrefs(2)
-                        recreate()
-                    }
-
-                    button3 = binding.button3
-                    button3.setOnClickListener { _ ->
-                        setPrefs(3)
-                        recreate()
-                    }
-
-                }
-
-
-            }
+        /*
+        button1 = binding.button1
+        button1.setOnClickListener { _ ->
+            setPrefs(1)
+            recreate()
         }
+
+        button2 = binding.button2
+        button2.setOnClickListener { _ ->
+            setPrefs(2)
+            recreate()
         }
+
+        button3 = binding.button3
+        button3.setOnClickListener { _ ->
+            setPrefs(3)
+            recreate()
+        }
+
+         */
+
+
+
+    }
+
+
+}
