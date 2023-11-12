9ff4097f94bf2129d88d4c52fbc4fc9ab8228404
Shadowproof <kubac400@gmail.com>
2023-11-12 17:15:02 +0100

    Error in OnCreateOptionsMenu and onOptionsItemSelected

--- app/src/main/java/com/example/l2_andro/ActivityLeft.kt
+++ app/src/main/java/com/example/l2_andro/ActivityLeft.kt
@@ -3,7 +3,7 @@ package com.example.l2_andro
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class activityRed : AppCompatActivity() {
+class ActivityLeft : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_left)
--- app/src/main/java/com/example/l2_andro/MainActivity.kt
+++ app/src/main/java/com/example/l2_andro/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import androidx.appcompat.widget.Toolbar
 import com.example.l2_andro.databinding.ActivityMainBinding
@@ -14,14 +16,15 @@ class MainActivity : AppCompatActivity() {
     lateinit var button2: Button
     lateinit var button3: Button
     lateinit var toolbar1: Toolbar
-
+    lateinit var menu1: Menu
+    //fun OnCreateOptionsMenu(menu: Menu!):Boolean
     fun applyTheme(){
         val data: SharedPreferences = getSharedPreferences("my_prefs", Context.MODE_PRIVATE)
         var themeNum = data.getInt("theme_color", 0)
         when (themeNum){
             1 -> setTheme(R.style.AppThemeRed)
             2 -> setTheme(R.style.AppThemeGreen)
-            3 -> setTheme(R.style.Theme_L2_andro)
+            3 -> setTheme(R.style.AppThemeGrey)
             else -> setTheme(R.style.Theme_L2_andro)
         }
     }
@@ -32,37 +35,62 @@ class MainActivity : AppCompatActivity() {
         editor.apply()
 
     }
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        applyTheme()
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
 
-        setContentView(R.layout.activity_main)
-
-        toolbar1 = binding.toolbar1
-        setSupportActionBar(toolbar1)
-
-        button1 = binding.button1
-        button1.setOnClickListener { _ ->
-            println("button1 clicked")
-            setPrefs(1)
-            recreate()
+    override fun OnCreateOptionsMenu(menu: Menu):Boolean{
+        menuInflater.inflate(R.menu.app_menu, menu)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem):Boolean{
+        return when (item.itemId) {
+            case R.id.mi0 -> {
+                true
+            }
+            case R.id.mi1 -> {
+                true
+            }
+            case R.id.mi2 -> {
+                true
+            }
+            case R.id.mi3 -> {
+                true
+            }
+            else -> super.onContextItemSelected(item)
         }
-
-        button2 = binding.button2
-        button2.setOnClickListener { _ ->
-            setPrefs(2)
-            recreate()
-        }
-
-        button3 = binding.button3
-        button3.setOnClickListener { _ ->
-            setPrefs(3)
-            recreate()
-        }
-
     }
 
+                override fun onCreate(savedInstanceState: Bundle?) {
+                    super.onCreate(savedInstanceState)
+                    applyTheme()
+                    binding = ActivityMainBinding.inflate(layoutInflater)
+                    setContentView(binding.root)
 
-}
+                    setContentView(R.layout.activity_main)
+
+                    toolbar1 = binding.toolbar1
+                    setSupportActionBar(toolbar1)
+
+                    button1 = binding.button1
+                    button1.setOnClickListener { _ ->
+                        println("button1 clicked")
+                        setPrefs(1)
+                        recreate()
+                    }
+
+                    button2 = binding.button2
+                    button2.setOnClickListener { _ ->
+                        setPrefs(2)
+                        recreate()
+                    }
+
+                    button3 = binding.button3
+                    button3.setOnClickListener { _ ->
+                        setPrefs(3)
+                        recreate()
+                    }
+
+                }
+
+
+            }
+        }
+        }
